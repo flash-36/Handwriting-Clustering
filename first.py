9ae9efed1f4ed461355d98ea71815ad0e5b8d50e
flash-36 <37663526+flash-36@users.noreply.github.com>
2018-06-06 14:22:18 +0530

    Add files via upload

--- first.py
+++ first.py
@@ -1,12 +1,13 @@
 import cv2
-import os
-import pandas as pd
+import os, shutil
 import numpy as np
 from sklearn.cluster import KMeans
-#import matplotlib.pyplot as plt
 
-folder="/home/ujwal/Downloads/dataset"
-cno=8
+
+source_folder="/media/ujwal/My files/Work/ML/Computer Vision/Handwriting Clustering/Input"
+dest_folder="/media/ujwal/My files/Work/ML/Computer Vision/Handwriting Clustering/Output"
+cno=10
+mindim=600
 def load_dataset(folder):
     data = {}
     for filename in os.listdir(folder):
@@ -22,23 +23,42 @@ def load_images_from_folder(folder):
     for filename in os.listdir(folder):
         img = cv2.imread(os.path.join(folder,filename))
         if img is not None:
-            data.update({filename:np.array(img[0:585,0:585]).ravel()})#crop every image to 585x585 and roll out and add to dict
+            data.update({filename:np.array(img[0:mindim,0:mindim]).ravel()})#crop every image to mindimxmindim and roll out and add to dict
     return data
 
-def dist(a, b):
-    return np.linalg.norm(a-b)
+def removeFolder(folder):
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path): 
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(e)
 
+def createFolder(directory):
+    try:
+    
+        os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory. ' + directory)
 
 def main():
 
-    placeholder = load_dataset(folder)
-    data = load_images_from_folder(folder)
+    placeholder = load_dataset(source_folder)
+    data = load_images_from_folder(source_folder)
     
 
     kmeans = KMeans(n_clusters = cno,random_state = 0).fit(list(data.values()))
+    
+
     fol=[]
+    removeFolder(dest_folder) #flush out previous outputs
     for i in range(cno):
-    	fol.append(folder+"/Cluster"+str(i+1))
+        createFolder(dest_folder+"/Cluster "+str(i+1))
+        fol.append(dest_folder+"/Cluster "+str(i+1))
+
     a=list(placeholder.values())
     b=list(placeholder.keys())
     for i in range(len(kmeans.labels_)):
@@ -49,8 +69,7 @@ def main():
     	
 
 
-    #for i in images:
-    #	print(i.shape)
+    
 
 
 
